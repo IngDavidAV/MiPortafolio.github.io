64e26dc3a6b2d15d08541ca2549a152aa3bb6aa7
David Avalos <dlconsultingcr@gmail.com>
2022-11-09 10:32:53 -0600

    Update correo.php

--- correo.php
+++ correo.php
@@ -1,17 +1,28 @@
 <?php 
-$destino = "davidav.dev@gmail.com";
+
 $nombre = $_POST["name"];
 $phone = $_POST["phone"];
 $email = $_POST["email"];
 $consulta = $_POST["consulta"];
 $mensaje = $_POST["mensaje"];
-$contenido = "Nombre: ".$nombre.
-			 "\nTeléfono: ".$phone.
-			 "\nCorreo: ".$email.
-			 "\nConsulta: ".$consulta.
-			 "\nMensaje: ".$mensaje;
 
-mail($destino, "Contacto", $contenido);
+$header = 'From'.$email."\r\n";
+$header .= "X-Mailer: PHP/".phpversion()."\r\n";
+$header .= "Mime-Version: 1.0 \r\n";
+$header .= "Content-Type: text/plain";
+
+$message = "Mensaje enviado por: ".$nombre."\r\n";
+$message .= "Teléfono de Contacto: ".$phone."\r\n";
+$message .= "Correo: ".$email."\r\n";
+$message .= "Consulta".$consulta."\r\n";
+$message .= "Mensaje".$_POST["mensaje"]."\r\n";
+$message .= "Enviado el: ".date('d/m/Y', time());
+
+
+$para = "davidav.dev@gmail.com";
+$asunto = "Asunto del mensaje";
+
+mail($para, $asunto, utf8_decode($message), $header);
 header("location:gracias.html");
 
 ?>
